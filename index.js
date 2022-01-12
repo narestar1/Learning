e43d6f5d351e61faa3a1ecc422597a4e156f60a0
Naresh <narestar1@gmail.com>
2022-01-12 23:55:39 +0530

    new docker-compose

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const redis = require('redis')
 
 const app = express();
-const client = redis.createClient();
-
+const client = redis.createClient({
+    host:'redis-server',
+    port: 6379
+});
+client.set('visists',0);
 
 app.get('/', (req,res) => {
     client.get('visits',(err,visits) => {
