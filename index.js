1c1885fb91a2bf88c60af5088234a157b507fe39
Naresh <narestar1@gmail.com>
2022-01-12 23:31:51 +0530

    no node_modules

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const redis = require('redis')
 
 const app = express();
+const client = redis.createClient();
+
 
 app.get('/', (req,res) => {
-    res.send('Hi agian from node')
+    client.get('visits',(err,visits) => {
+        res.send('Page has been visited for ' + visits);
+        client.set('visits', parseInt(visits) + 1)
+    })
+    
 })
 
-app.listen(8080, () => {
+app.listen(8081, () => {
     console.log('listening on port 8080')
 })
